2a9e39256e61e1ad0c2e4a497b39d6c8030492f5
digitalfarmer <adesbsp@gmail.com>
2020-01-24 15:05:21 +0700

    add compute field

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -9,12 +9,21 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _inherit = ['mail.thread','mail.activity.mixin']
     _description = 'Partient Record'
-    _rec_name = 'patient_name'
+    _rec_name = 'name_seq'
 
-    patient_name = fields.Char(string='Name', required=True)
-    patient_age = fields.Integer('Age')
-    notes = fields.Text(string='Notes')
-    image = fields.Binary(string='Photo')
+    @api.depends('patient_age')
+    def set_age_group(self):
+        for rec in self :
+            if rec.patient_age:
+                if rec.patient_age< 18:
+                    rec.age_group='minor'
+                else :
+                    rec.age_group='mayor'
+
+    patient_name = fields.Char(string='Name', required=True, track_visibility='always')
+    patient_age = fields.Integer('Age', track_visibility='always')
+    notes = fields.Text(string='Registrtion Notes')
+    image = fields.Binary(string='Image' , attachment=True)
     name = fields.Char(string='Test')
     # sequence fields
     name_seq = fields.Char(string='Patient ID',reuired=True, copy=False, readonly=True,
@@ -24,6 +33,11 @@ class HospitalPatient(models.Model):
         ('female', 'Female'),
         ('trans', 'Transgender')
     ],default='male',string='Gender')
+    age_group = fields.Selection([
+        ('mayor','Mayor'),
+        ('minor','Minor')
+    ],string='Age Group', compute='set_age_group')
+
     # sequence label
     @api.model
     def create(self, vals):
