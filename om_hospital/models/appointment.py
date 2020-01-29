11022bd656c438359c834e6feba65e2a5fdaf373
digitalfarmer <adesbsp@gmail.com>
2020-01-29 15:07:32 +0700

    adding controlling state status bar

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -6,6 +6,14 @@ class HospitalAppointment(models.Model):
     _inherit = ['mail.thread','mail.activity.mixin']
     _order = 'id desc'
 
+    def action_confirm(self):
+        for rec in self:
+            rec.state='confirm'
+
+    def action_done(self):
+        for rec in self:
+            rec.state='done'
+
     @api.model
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
